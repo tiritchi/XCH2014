eaff8de2e161a293940559c07cff964251c2d240
Cédric <tiritchi@gmail.com>
2014-02-09 19:05:06 +0100

    rarar

--- register/testdb.php
+++ register/testdb.php
@@ -9,31 +9,14 @@
 	{
 		$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'raspberry');
 		echo '<p> connected to DB test</p>';
-		
-		$bdd->exec("INSERT INTO users(fname,lname,mail,phone,sexe,date) VALUES('test','test,'aa@aa.com','0621849218','Male','')");
-		$reponse = $bdd->query('SELECT * FROM users');
-		while($donnees = $reponse->fetch())
-		{
-			?>
-			<p>
-		    	<?php echo $donnees['fname']; ?><br/>;
-		    	<?php echo $donnees['lname']; ?><br/>;
-		    	<?php echo $donnees['mail']; ?><br/>;
-		    	<?php echo $donnees['phone']; ?><br/>;
-		    	<?php echo $donnees['sexe']; ?><br/><br/>;
-			</p>
-			<?php
-		}
-		$reponse -> closeCursor();
-		?>
-
-		echo '<p>l utilisateur a ete rajouté</p>';
 	} 
 	catch (Exception $e) 
 	{
 		die('Erreur : ' . $e->getMessage());
 		echo '<p> erreur DB</p>';
 	} 
+	$bdd->exec('INSERT INTO users (fname,lname,mail,phone,sexe,date) VALUES ('test','test2','aa@.com','62065206','Male','')');
+	echo '<p> inserted</p>';
 
 ?>
 <html>
