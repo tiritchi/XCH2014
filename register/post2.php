efb722c848699b847569d8d5d42444a2f8e630a3
Cédric <tiritchi@gmail.com>
2014-02-09 16:55:22 +0100

    re

--- register/post2.php
+++ register/post2.php
@@ -10,21 +10,22 @@
 	{
 		$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'raspberry');
 		echo '<p> connected to DB</p>';
+		
+		if (isset($_POST['password']) AND $_POST['password'] == "test")
+		{
+			echo '<p> good password</p>';
+			$bdd = exec('INSERT INTO users(id,fname,lname,mail,phone,sexe,date) VALUES('',"test","test","test","test","test",'')');
+
+		}
+		else
+		{
+			echo '<p> wrong password</p>';
+		}
 	} 
 	catch (Exception $e) 
 	{
 		die('Erreur : ' . $e->getMessage());
 		echo '<p> erreur DB</p>';
-	}
-	if (isset($_POST['password']) AND $_POST['password'] == "test")
-	{
-		echo '<p> good password</p>';
-//		$bdd = exec('INSERT INTO users(id,fname,lname,mail,phone,sexe,date) VALUES('',"test","test","test","test","test",'')');
-
-	}
-	else
-	{
-		echo '<p> wrong password</p>';
 	}
 ?>
 </html>
