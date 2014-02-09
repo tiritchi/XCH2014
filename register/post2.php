c098b37644e9b548ae6a0a3a69aeae444d80339f
Cédric <tiritchi@gmail.com>
2014-02-09 16:34:24 +0100

    test new

---
+++ register/post2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
+</head>
+<?php
+	try 
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'raspberry');
+		echo '<p> connected to DB</p>';
+	} 
+	catch (Exception $e) 
+	{
+		die('Erreur : ' . $e->getMessage());
+		echo '<p> erreur DB</p>';
+	}
+?>
+</html>
