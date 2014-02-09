bd4543c8e7bef985d7120981bab0317f0a895a03
Cédric <tiritchi@gmail.com>
2014-02-09 16:52:27 +0100

    re

--- register/post2.php
+++ register/post2.php
@@ -16,28 +16,13 @@
 		die('Erreur : ' . $e->getMessage());
 		echo '<p> erreur DB</p>';
 	}
-	$reponse = $bdd->query('SELECT * FROM jeux_video WHERE possesseur="test" ');
 
-		// On affiche chaque entrée une à une
-	while ($donnees = $reponse->fetch())
-	{
-	?>
-	    <p>
-		    <strong>Jeu</strong> : <?php echo $donnees['nom']; ?><br />
-		    Le possesseur de ce jeu est : <?php echo $donnees['possesseur']; ?>, et il le vend à <?php echo $donnees['prix']; ?> euros !<br />
-		    Ce jeu fonctionne sur <?php echo $donnees['console']; ?> et on peut y jouer à <?php echo $donnees['nbre_joueurs_max']; ?> au maximum<br />
-		    <?php echo $donnees['possesseur']; ?> a laissé ces commentaires sur <?php echo $donnees['nom']; ?> : <em><?php echo $donnees['commentaires']; ?></em>
-		</p>
-		<?php
-	}
-
-	$reponse->closeCursor(); // Termine le traitement de la requête
-?>
 ?>
 <?php
 	if (isset($_POST['password']) AND $_POST['password'] == "test")
 	{
 		echo '<p> good password</p>';
+		$bdd = exec('INSERT INTO users(id,fname,lname,mail,phone,sexe,date) VALUES('',"test","test","test","test","test",'')');
 
 	}
 	else
