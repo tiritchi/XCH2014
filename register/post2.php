3dffea000c703eb1ab5fa5cb408dc690d77f7ed8
Cédric <tiritchi@gmail.com>
2014-02-09 16:43:18 +0100

    r

--- register/post2.php
+++ register/post2.php
@@ -21,6 +21,23 @@
 	if (isset($_POST['password']) AND $_POST['password'] == "test")
 	{
 		echo '<p> good password</p>';
+		$reponse = $bdd->query('SELECT * FROM jeux_video WHERE possesseur="test" ');
+
+		// On affiche chaque entrée une à une
+	while ($donnees = $reponse->fetch())
+	{
+		?>
+	    <p>
+		    <strong>Jeu</strong> : <?php echo $donnees['nom']; ?><br />
+		    Le possesseur de ce jeu est : <?php echo $donnees['possesseur']; ?>, et il le vend à <?php echo $donnees['prix']; ?> euros !<br />
+		    Ce jeu fonctionne sur <?php echo $donnees['console']; ?> et on peut y jouer à <?php echo $donnees['nbre_joueurs_max']; ?> au maximum<br />
+		    <?php echo $donnees['possesseur']; ?> a laissé ces commentaires sur <?php echo $donnees['nom']; ?> : <em><?php echo $donnees['commentaires']; ?></em>
+		</p>
+		<?php
+	}
+
+	$reponse->closeCursor(); // Termine le traitement de la requête
+?>
 	}
 	else
 	{
