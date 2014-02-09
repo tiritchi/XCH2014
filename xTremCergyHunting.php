66ac1211109731ce875588d0bef43cdab401c3f5
Cédric <tiritchi@gmail.com>
2014-02-09 20:12:44 +0100

    pass

--- xTremCergyHunting.php
+++ xTremCergyHunting.php
@@ -7,7 +7,7 @@
         <title>xTremCergyHunting</title>
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="signin.css"></link>
-        <?php require('includes/PasswordHash.php'); ?>
+        <?php require('lib/PasswordHash.php'); ?>
 
     </head>
     
