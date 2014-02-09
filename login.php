75e7354f77367e1c16f3e09e5d21eb724294d09f
Cédric <tiritchi@gmail.com>
2014-02-09 22:20:29 +0100

    aa

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
 	try 
 	{
 		$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', 'raspberry');
-//		echo '<p> connected to DB</p>';
+		echo '<p> connected to DB</p>';
 	} 
 	catch (Exception $e) 
 	{
@@ -22,6 +22,7 @@
 	$user = $_POST['email'];
 	$psswd= $_POST['password'];
 	$req=$bdd->query('SELECT psswd FROM users WHERE mail=/'$user/'');
+	echo '<p> DB reached</p>';
 	if($req==NULL)
 	{
 		echo '<p> Vous n\'êtes pas inscrit</p>';
