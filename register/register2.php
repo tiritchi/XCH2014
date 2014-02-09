986944c3308c8cfb415509f8c92ad057e5aff090
Cédric <tiritchi@gmail.com>
2014-02-09 11:45:59 +0100

    indentation register2

--- register/register2.php
+++ register/register2.php
@@ -34,59 +34,59 @@ if(isset($_POST['submitted']))
 </head>
 
 <body>
-<div class="col-lg-4 col-lg-offset-4">
-<div class="panel panel-primary">
-	<div class="panel-heading">
-    <h3 class="panel-title">Formulaire d'inscription</h3>
-  	</div>
-  	<div class="panel-body">
-  	<form class="form-horizontal">
-<fieldset>
+	<div class="col-lg-4 col-lg-offset-4">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+   				 <h3 class="panel-title">Formulaire d'inscription</h3>
+  			</div>
+  			<div class="panel-body">
+  				<form class="form-horizontal">
+				<fieldset>
 
-<!-- Text input-->
-<div class="input-group">
-	<span class="input-group-addon">First name</span>
-    <input id="First name" name="First name" placeholder="First name" class="form-control" required="" type="text">
-</div><br/>
+				<!-- Text input-->
+				<div class="input-group">
+					<span class="input-group-addon">First name</span>
+				    <input id="First name" name="First name" placeholder="First name" class="form-control" required="" type="text">
+				</div><br/>
 
-<!-- Text input-->
-<div class="input-group">
-    <span class="input-group-addon">Last name</span>
-    <input id="Last name" name="Last name" placeholder="Last name" class="form-control" required="" type="text">
-</div><br/>
+				<!-- Text input-->
+				<div class="input-group">
+				    <span class="input-group-addon">Last name</span>
+				    <input id="Last name" name="Last name" placeholder="Last name" class="form-control" required="" type="text">
+				</div><br/>
 
-<!-- Text input-->
-<div class="input-group">
-    <span class="input-group-addon">eMail</span>
-    <input id="Email" name="Email" placeholder="example@example.com" class="form-control" required="" type="text">
-</div><br/>
+				<!-- Text input-->
+				<div class="input-group">
+				    <span class="input-group-addon">eMail</span>
+				    <input id="Email" name="Email" placeholder="example@example.com" class="form-control" required="" type="text">
+				</div><br/>
 
-<!-- Text input-->
-<div class="input-group">
-    <span class="input-group-addon">Phone N°</span>
-    <input id="Phone N°" name="Phone N°" placeholder="06XXXXXXXX" class="form-control" required="" type="text">
-</div><br/>
+				<!-- Text input-->
+				<div class="input-group">
+				    <span class="input-group-addon">Phone N°</span>
+				    <input id="Phone N°" name="Phone N°" placeholder="06XXXXXXXX" class="form-control" required="" type="text">
+				</div><br/>
 
-<!-- Select Basic -->
-<div class="input-group">
-    <span class="input-group-addon">Sexe</span>
-    <select class="selectpicker">
-		<option <span class="glyphicon glyphicon-search"></span> Male</option>
-		<option <span class="glyphicon glyphicon-search"></span> Female</option>
-	</select>
-</div><br/>
+				<!-- Select Basic -->
+				<div class="input-group">
+				    <span class="input-group-addon">Sexe</span>
+				    <select class="selectpicker">
+						<option Male</option>
+						<option Female</option>
+					</select>
+				</div><br/>
 
-<!-- Button -->
-<div class="col-lg-offset-4">
-<div class="input-group">
-	<button id="Submit" name="Submit" class="btn btn-primary">Submit</button>
-</div>
-</div>
+				<!-- Button -->
+				<div class="col-lg-offset-4">
+					<div class="input-group">
+						<button id="Submit" name="Submit" class="btn btn-primary">Submit</button>
+					</div>
+				</div>
 
-</fieldset>
-</form>
-  	</div>
-</div>
-</div>
+				</fieldset>
+				</form>
+  			</div>
+		</div>
+	</div>
 </body>
 </html>
