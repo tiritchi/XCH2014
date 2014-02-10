2b450d368a07484c3ccf7908154865fba4363d89
Cédric <tiritchi@gmail.com>
2014-02-10 11:22:02 +0100

    a

--- testdb.php
+++ testdb.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <head>
-	<title>test db</title>
+	<title>test db/title>
 	<meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../signin.css"></link>   
 </head>
 <?php
 	try 
