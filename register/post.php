763581039150fdaa8807670b80d36539578bc335
Cédric <tiritchi@gmail.com>
2014-02-10 11:56:29 +0100

    aa

--- register/post.php
+++ register/post.php
@@ -39,7 +39,7 @@
 
 		//envoie des informations à la DB
 
-		$req = $bdd->prepare('INSERT INTO users (group,fname,lname,mail,phone,sexe,psswd,reg_date) VALUES ('',:prenom,:nom,:email,:phone,:sexe,:psswd,NOW())');
+		$req = $bdd->prepare('INSERT INTO users (group,fname,lname,mail,phone,sexe,psswd,reg_date) VALUES (\'\',:prenom,:nom,:email,:phone,:sexe,:psswd,NOW())');
 		$req->execute(array(
 		    'nom' => $nom,
 		    'prenom' => $prenom,
