df127dc35e633fabcf39524983a3892a7b36087f
Cédric <tiritchi@gmail.com>
2014-02-10 22:02:42 +0100

    aa

--- include/manager/admin/header.php
+++ include/manager/admin/header.php
@@ -27,12 +27,11 @@
 			                        <span class="icon-bar"></span>
 			                        <span class="icon-bar"></span>
 			                    </button>
-			                    <a class="navbar-brand" href="#">Brand</a>
 			                </div>
 
 			                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			                    <ul class="nav navbar-nav">
-			                        <li><a href="#">Home</a></li>
+			                        <li><a href="index?page=admin">Tableau de Bord</a></li>
 			                        <li><a href="#">Profile</a></li>
 			                        <li class="dropdown">
 			                            <a href="#" data-toggle="dropdown" class="dropdown-toggle">Messages <b class="caret"></b></a>
