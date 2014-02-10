5f7a4e72648b6a5984fc646caccfe2f634531da5
Cédric <tiritchi@gmail.com>
2014-02-10 01:38:43 +0100

    aa

--- manager/board.php
+++ manager/board.php
@@ -7,41 +7,14 @@ session_start(); // On démarre la session AVANT toute chose
 	<meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../signin.css"></link>
-    $('.dropdown-toggle').dropdown()
- 
+    <link rel="stylesheet" href="../signin.css"></link> 
 </head>
 <html>
-	<nav class="navbar navbar-default" role="navigation">
-		<div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#">Brand</a>
-		    </div>
-
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="#">Link</a></li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['user']; ?><b class="caret"></b></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		            <li class="divider"></li>
-		            <li><a href="#">Separated link</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		    </div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
-	</nav>
+	<div class="jumbotron">
+		<h1>Hello, world!</h1>
+			<p class="navbar-text navbar-right">Signed in as <a href="#" class="navbar-link"><?php echo $_SESSION['mail']?></a></p>
+		<p><a class="btn btn-primary btn-lg" role="button">Learn more</a></p>
+	</div>
 
 	<script src="../bootstrap/js/bootstrap.min.js"></script>
 </html>
