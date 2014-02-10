0b309a177f7ae66cf1be5239989852ee08b5a1d8
Cédric <tiritchi@gmail.com>
2014-02-10 19:02:02 +0100

    aa

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
  
 if (isset($_GET['page']) && $_GET['page'] != "")
 {
-    $path = $_GET['page']."php";
+    $path = $_GET['page'].".php";
     if (file_exists($path))
     {
         require_once($path);
