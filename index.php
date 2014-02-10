ba5960d6c1dde66a88c14113324add736e1446b6
Cédric <tiritchi@gmail.com>
2014-02-10 18:50:10 +0100

    oo

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+ 
+if (isset($_GET['page']) && $_GET['page'] != "")
+{
+    $path = "pages/".$_GET['page'];
+    if (file_exists($path))
+    {
+        require_once("pages/".$_GET['page'].".php");
+    }
+    else
+    {
+        require_once("pages/404.php");
+    }
+}
+else
+{
+    header('Location: ./index.php?page=xTremCergyHunting.php');
+}
+ 
+?>
