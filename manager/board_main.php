2c3b0923c814ed757754f4611bc1dba09358226b
Cédric <tiritchi@gmail.com>
2014-02-10 10:37:39 +0100

    aa

--- manager/board_main.php
+++ manager/board_main.php
@@ -2,7 +2,7 @@
 session_start();
 if(isset($_SESSION['connected']) && $_SESSION['connected']=TRUE)
 	{
-		echo file_get_contents('board_main.php');
+		echo file_get_contents('board.php');
 	}
 else
 {
