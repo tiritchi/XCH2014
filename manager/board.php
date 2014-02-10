e8133d88bb222367fbcc48aa46109bd8aa2078e6
Cédric <tiritchi@gmail.com>
2014-02-10 10:28:04 +0100

    a

--- manager/board.php
+++ manager/board.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(!isset($_SESSION['connected']) || !$_SESSION['connected']=TRUE){
+if(isset($_SESSION['connected']) && $_SESSION['connected']=TRUE){
 	echo '<meta http-equiv="refresh" content="0; url=xTremCergyHunting.php">';
 	echo '<!--';
 } 
