58c83d704ad0fd685192c491b58bf8afaf6a4bb8
jnconte <jeannoel.conte@gmail.com>
2014-02-12 22:01:22 +0100

    a

--- include/manager/board.php
+++ include/manager/board.php
@@ -112,13 +112,13 @@ else
 			    <div class="modal-content">
 			      <div class="modal-body">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <img src="ressources/profile.jpg" class="img-responsive" alt="Responsive image" style="height: 180px; width: 100%; display: block;"></img>
+                    <img src="ressources/profile.jpg" alt="100%x180" style="height: 180px; width: 100%; display: block;"></img>
 			      </div>
 			      <div class="modal-footer">
                     <div class="input-group">
                        <input class="form-control" type="text" placeholder="Enter target number here..."></input>
                           <span class="input-group-btn">
-                          <button class="btn btn-default" type="button">Go!</button>
+                          <button class="btn btn-default" type="button">Submit</button>
                        </span>
                     </div>
                   </div>
