accd6a606c18308850376935702a910346cd29fb
Cédric <tiritchi@gmail.com>
2014-02-13 00:55:22 +0100

    nouveau

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php require('include/lib/functions.php'); ?>
 <?php 
 	$bdd=db_init();
-	$cno=create_contract($bdd,"37","36","2014-05-06");
+//	$cno=create_contract($bdd,"37","36","2014-05-06");
 //		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (?,?,?,?,?,NOW())');
 //		$req->execute(array('X32235C65H14','32','33','0','2014-06-12'));
 
