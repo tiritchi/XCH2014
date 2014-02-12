1e477548f19201346ac3bce702fb822d8e5d2282
jnconte <jeannoel.conte@gmail.com>
2014-02-12 18:11:40 +0100

    Board user
    v4

--- include/manager/board.php
+++ include/manager/board.php
@@ -32,7 +32,7 @@ else
 			    
 			    
 			    
-				<div class="col-md-4>
+				<div class="col-md-4">
 					<div class="panel panel-default">
 						<div class="panel-heading">
 						    <h3 class="panel-title">Profile</h3>
