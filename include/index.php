6a40efdab1cdcd4e0505eeb7bea8906f04f6dec7
Cédric <tiritchi@gmail.com>
2014-02-12 13:18:59 +0100

    nouveau

--- include/index.php
+++ include/index.php
@@ -13,7 +13,7 @@
             .bs-example{
                 margin: 20px;
             }
-            body { padding-top: 70px; }
+            body { padding-top: 50px; }
         </style>
     </head>
     
