45c48ccdc096d67659d3a13ace40b752d86d7605
Cédric <tiritchi@gmail.com>
2014-02-12 21:43:39 +0100

    nouveau

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
 <?php require('include/lib/db.php'); ?>
 <?php 
 	$bdd=db_init();
-	create_contract($bdd,"32","33","2014-05-06");
+//	create_contract($bdd,"32","33","2014-05-06");
+		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (?,?,?,?,?,NOW())');
+		$req->execute(array('X32235C65H14','32','33','2014-06-122'));
+		echo 'test';
  ?> 
