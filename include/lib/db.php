f04432d37881afcf6aa6a1e1c43bf75f213566a4
Cédric <tiritchi@gmail.com>
2014-02-12 21:37:53 +0100

    nouveau

--- include/lib/db.php
+++ include/lib/db.php
@@ -62,12 +62,12 @@
 
 	function create_contract($bdd,$user_id,$target_id,$exp_date){
 		srand();
-		$contract_no='X'.rand(10000,99999).'C'.$target_id.'H14';
+		$cno='X'.rand(10000,99999).'C'.$target_id.'H14';
 		$uid = intval($user_id);
 		$tid = intval($target_id);
-//		$req = $bdd->exec('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (,$uid,$tid,\'0\',$exp_date,NOW())');
-		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (:contract_no,:uid,:tid,:complete,:expd,NOW()');
-		$req->execute(array('uid'=>$uid,'tid'=>$tid,'complete'=>'0','expd'=>$exp_date,'contract_no'=>$contract_no));
+		$req = $bdd->exec('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES ($cno,$uid,$tid,\'0\',$exp_date,NOW())');
+//		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (:contract_no,:uid,:tid,:complete,:expd,NOW()');
+//		$req->execute(array('uid'=>$uid,'tid'=>$tid,'complete'=>'0','expd'=>$exp_date,'contract_no'=>$contract_no));
 	    echo $contract_no;
 		return 0;
 	}
