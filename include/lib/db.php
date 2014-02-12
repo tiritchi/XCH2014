3d47a4c9853f8da4925867d6b78e57060c6befe2
Cédric <tiritchi@gmail.com>
2014-02-12 21:48:48 +0100

    nouveau

--- include/lib/db.php
+++ include/lib/db.php
@@ -66,7 +66,7 @@
 		$uid = intval($user_id);
 		$tid = intval($target_id);
 		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (?,?,?,?,?,NOW())');
-		$req->execute(array($cno,$user_id,$target_id,$exp_date));
+		$req->execute(array($cno,$user_id,$target_id,'0',$exp_date));
 //		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (:contract_no,:uid,:tid,:complete,:expd,NOW()');
 //		$req->execute(array('uid'=>$uid,'tid'=>$tid,'complete'=>'0','expd'=>$exp_date,'contract_no'=>$contract_no));
 	    echo $contract_no;
