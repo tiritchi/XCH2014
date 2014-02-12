ea3a56b24be309d1f12eaa9de52296e4269d22ed
jnconte <jeannoel.conte@gmail.com>
2014-02-12 18:10:45 +0100

    Board user
    v3

--- include/manager/board.php
+++ include/manager/board.php
@@ -11,7 +11,7 @@ else
 		?>
 		<div class="jumbotron">
 			<div class="row">
-				<div class="col-md-6">
+				<div class="col-md-8">
 			        <div class="panel panel-default">
 			            <div class="panel-heading">
 			                <h3 class="panel-title">Contracts</h3>
@@ -32,16 +32,24 @@ else
 			    
 			    
 			    
-				<div class="col-md-6">
+				<div class="col-md-4>
 					<div class="panel panel-default">
 						<div class="panel-heading">
 						    <h3 class="panel-title">Profile</h3>
 						</div>
 						<div class="panel-body">
 						    <ul class="list-group">
-						        <li class="list-group-item">pseudo</li>
-						        <li class="list-group-item">10 avenue de la mer</li>
-						        <li class="list-group-item">CERGY</li>
+						        <li class="list-group-item">Nickname</li>
+						        <li class="list-group-item">School</li>
+						        <li class="list-group-item">First name</li>
+						        <li class="list-group-item">LAST NAME</li>
+						        <li class="list-group-item">JJ/MM/AAAA</li>
+						        <li class="list-group-item">Address</li>
+						        <li class="list-group-item">Post code</li>
+						        <li class="list-group-item">CITY</li>
+						        <li class="list-group-item">e-mail</li>
+						        <li class="list-group-item">Phone number</li>
+						        <li class="list-group-item">Sex</li>
 						    </ul>
 						</div>
 					</div>
