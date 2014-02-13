63a74286caf71728e4c6b93ddd4329820c842add
jnconte <jeannoel.conte@gmail.com>
2014-02-13 20:39:40 +0100

    azer

--- include/manager/board.php
+++ include/manager/board.php
@@ -122,7 +122,7 @@ else
 					      <div class="modal-body">
 		                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 		                        <div class="page-header">
-		                            <h1><img src="ressources/profile_resized.jpg" class="img-thumbnail"></img>  '.$tar[0].'  <br />'.$ar[3].'</h1>
+		                            <h1><img src="ressources/profile_resized.jpg" class="img-thumbnail"></img>  '.$ar[3].'  <small>'.$tar[0].'</small></h1>
                                 </div>
 					      </div>
 					      <div class="modal-footer">
