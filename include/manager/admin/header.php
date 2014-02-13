26d5057844c0e95846ca30ae9fbff428d7756681
Cédric <tiritchi@gmail.com>
2014-02-13 23:07:00 +0100

    test

--- include/manager/admin/header.php
+++ include/manager/admin/header.php
@@ -31,7 +31,7 @@
 	                    <ul class="nav navbar-nav">
 	                        <li><a href="#">Home</a></li>
 	                        <li><a href="#">Profile</a></li>
-	                        <li><a href="#" data-toggle="dropdown" data-toggle="modal" data-target="#mail">Mail</a></li>
+	                        <li><a href="" data-toggle="dropdown" data-toggle="modal" data-target="#mail">Mail</a></li>
 	                        
 	                    </ul>
 	                    <ul class="nav navbar-nav navbar-right">
@@ -48,4 +48,3 @@
 	                </div>
 	            </div>
 	        </div>
-	        
