4e992b441680ba13ae943ca1d29d8c999f667598
Cédric <tiritchi@gmail.com>
2014-02-13 21:59:32 +0100

    test

--- include/lib/functions.php
+++ include/lib/functions.php
@@ -140,4 +140,45 @@
 		$u_code.='XCH14';
 		return $u_code;
 	}
+
+	function send_mail($ident,$pass,$user,$for_who){
+		require 'include/lib/phpmailer/PHPMailerAutoload.php';
+
+		$mail = new PHPMailer;
+
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host = 'smtp.gmail.com';  // Specify main and backup server
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = $ident;                            // SMTP username
+		$mail->Password = $pass;                           // SMTP password
+		$mail->SMTPSecure = 'tls';
+		$mail->Port = 587;
+		$mail->SMTPDebug = 1;                            // Enable encryption, 'ssl' also accepted
+
+		$mail->From = $user;
+		$mail->FromName = $user;
+		$mail->addAddress($for_who);  // Add a recipient
+		//$mail->addAddress('ellen@example.com');               // Name is optional
+//		$mail->addReplyTo('');
+		//$mail->addCC('tiritchi@gmail.com');
+		//$mail->addBCC('bcc@example.com');
+
+		$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+		//$mail->addAttachment('/var/www/XCH2014/testmail.php');         // Add attachments
+		//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		$mail->isHTML(true);                                  // Set email format to HTML
+
+		$mail->Subject = $subject;
+		$mail->Body    = $body;
+		$mail->AltBody = $body;
+
+		if(!$mail->send()) {
+		   echo 'Message could not be sent.';
+		   echo 'Mailer Error: ' . $mail->ErrorInfo;
+		   exit;
+		}
+
+		echo 'Message has been sent';
+
+	}
 ?>
