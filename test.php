5caf315ccb8ae08aaab44dbfd59d56656da1f78d
Cédric <tiritchi@gmail.com>
2014-02-13 01:22:36 +0100

    nouveau

--- test.php
+++ test.php
@@ -1,10 +1,10 @@
 <?php require('include/lib/functions.php'); ?>
 <?php 
 	$bdd=db_init();
-	$cno=create_contract($bdd,"37","36","2014-05-06");
+//	$cno=create_contract($bdd,"37","36","2014-05-06");
 //		$req = $bdd->prepare('INSERT INTO contracts (contract_no,user_id,target_id,complete,exp_date,start_date) VALUES (?,?,?,?,?,NOW())');
 //		$req->execute(array('X32235C65H14','32','33','0','2014-06-12'));
 
 //		echo gen_user_code('ENSEA','Male',"0621849218");
-		echo mark_as_complete($bdd,$cno,"03X90724XCH14");
+		echo mark_as_complete($bdd,"X99709C36H1477","03X90724XCH14");
  ?> 
