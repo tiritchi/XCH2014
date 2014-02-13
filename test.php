f839976420da45ae82a4b8338e728df380d5c71b
Cédric <tiritchi@gmail.com>
2014-02-13 03:01:36 +0100

    nouveau

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
 //		echo gen_user_code('ENSEA','Male',"0621849218");
 	//mark_as_complete($bdd,13);
 	$array=get_contracts($bdd,37);
-	$n=count($array)
+	$n=count($array);
 	for($i=0;$i<=$n;$i++){
 		echo array[$i][0]."-".array[$i][1]."-".array[$i][2]."-".array[$i][3]."-".array[$i][4];
 	}
