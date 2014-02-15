08e908a955d4f671ba460d600568dc4559f8411a
Cédric <tiritchi@gmail.com>
2014-02-15 20:47:43 +0100

    test

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
     $alfa='abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
     echo substr(str_shuffle($alfa),0,30);
-    echo'<html><body><img data-src="ressources/map.jpg" alt="..."></body></html>';
 ?>
+<!DOCTYPE html>
+<html><body><img data-src="ressources/map.jpg" alt="..."></body></html>
