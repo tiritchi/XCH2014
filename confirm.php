ecac9dacbd2dd9ecbfcb78b9a6ebb043aa9f1491
Cédric <tiritchi@gmail.com>
2014-02-17 13:20:32 +0100

    test

--- confirm.php
+++ confirm.php
@@ -6,8 +6,8 @@ if (isset($_GET['code']) && $_GET['code'] != "" && isset($_GET['mail']) && $_GET
 	$stat=confirm_account($_GET['mail'],$_GET['code']);
 	if($stat=='TRUE'){
 		echo 'Your account is confirmed<br/> You can now login in your personnal account threw the home page <br/>';
-		echo 'redirection sur la page d\'accueil dans 5 sec<br/>';
-        echo '<meta http-equiv="refresh" content="5; url=.">';
+		echo 'redirection sur la page d\'accueil dans 8 sec<br/>';
+        echo '<meta http-equiv="refresh" content="8; url=.">';
 	}
 	else{
 		echo $stat;
