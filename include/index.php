ef31cda2266901abbb6cad4c9254935b56e2fe71
Cédric <tiritchi@gmail.com>
2014-02-18 00:14:03 +0100

    test

--- include/index.php
+++ include/index.php
@@ -114,6 +114,7 @@
                             <p>Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui. Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth.</p>
                         </div>
                     </div>
+                    </div>
                     <div class="col-lg-4">
                         <div class="panel panel-default">
                             <div class="panel-heading">
