a68b3213fb6ea4ccceb72b55f8cf6577821eaf38
Cédric <tiritchi@gmail.com>
2014-02-17 17:33:15 +0100

    test

--- include/manager/header.php
+++ include/manager/header.php
@@ -38,8 +38,6 @@
 	                    	<li class="dropdown">
 	                            <a href="#" data-toggle="dropdown" class="dropdown-toggle"><i class="glyphicon glyphicon-user"></i> <?php echo $_SESSION['user']?>		<b class="caret"></b></a>
 	                            <ul class="dropdown-menu btn-toolbar">
-	                                <li><a href="#"><i class="glyphicon glyphicon-cog"></i>Settings</a></li>
-	                                <li class="divider"></li>
 	                                <li><a href="logout"><i class="glyphicon glyphicon-log-out "></i>Log out</a></li>
 	                            </ul>
 	                        </li>
