92808a7e6cd06de17e39ade42daa8ab3c9fc2060
Cédric <tiritchi@gmail.com>
2014-02-17 22:04:17 +0100

    test

--- include/index.php
+++ include/index.php
@@ -52,51 +52,18 @@
                         <a class="btn btn-primary btn-sm" role="button" href="register">Sign up !</a>
                     </form>
                 </div>
-                <!--
-
-                /.navbar-collapse 
-
-                -->
             </div>
-        </div>
-            
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                <img src="ressources/img_1.jpg" alt="pouet">
-                    <div class="carousel-caption">
-                        ...
-                    </div>
-                </div>
-                <div class="item">
-                <img src="ressources/img_2.png" alt="pouet">
-                    <div class="carousel-caption">
-                        ...
-                    </div>
-                </div>
-                <div class="item">
-                <img src="ressources/img_3.jpg" alt="pouet">
-                    <div class="carousel-caption">
-                        ...
-                    </div>
-                </div>
+      
+        <div class="row">
+            <div class=" col-lg-6">
+                <a class="thumbnail">
+                    <img src="ressources/home.jpg" alt="...">
+                </a>
             </div>
-        
-          <!-- Controls -->
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
+            <div class="jumbotron">
+            blabla
+
+            </div>
         </div>
     </body>
 </html>
