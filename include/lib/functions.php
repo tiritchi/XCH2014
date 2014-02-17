5f9765fc3024872ffc6f1ea5991eb29c154914f4
Cédric <tiritchi@gmail.com>
2014-02-18 00:28:34 +0100

    test

--- include/lib/functions.php
+++ include/lib/functions.php
@@ -312,7 +312,7 @@
 
 	function get_alives(){
 		$bdd=db_init();
-		$req=$bdd->query('SELECT alive FROM XCH14_users WHERE group=''user''');
+		$req=$bdd->query("SELECT alive FROM XCH14_users WHERE group='user'");
 		$alive='0';
 		$all='0';
 		while ($data=$req->fetch()){
