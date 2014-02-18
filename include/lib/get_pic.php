c85d1d4371efb8886c8d5100eecbfd0bc8ec2073
Cédric <tiritchi@gmail.com>
2014-02-18 02:02:09 +0100

    test

--- include/lib/get_pic.php
+++ include/lib/get_pic.php
@@ -3,7 +3,7 @@ session_start();
 if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SESSION['admin']) && $_SESSION['admin']=='FALSE')
 	{
 		if(isset($_GET['pseudo']) && $_GET['pseudo'] != ""){
-			$file = 'ressources/1/'.$_GET['pseudo'].'.jpg';
+			$file = '../../ressources/1/'.$_GET['pseudo'].'.jpg';
 			$type = 'image/jpeg';
 			header('Content-Type:'.$type);
 			header('Content-Length: ' . filesize($file));
