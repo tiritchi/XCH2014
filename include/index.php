5793690225eedda988e2d5f2e4db0bca7a3d3457
Cédric <tiritchi@gmail.com>
2014-02-18 01:47:49 +0100

    test

--- include/index.php
+++ include/index.php
@@ -88,26 +88,7 @@
                     </div>
                     </div>
                     <div class="col-lg-4">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">Top 5 players</h3>
-                            </div>
-                            <div class="panel-body">
-                                <ul class="list-group">
-                                    <?php
-                                        $arr=array();
-                                        $arr=get_hs();
-                                        foreach ($arr as $data) {
-                                            echo '<li class="list-group-item">'.$data[0].' <span class="badge">'.$data[1].' points</span></li>';
-                                        }
-                                    ?>
-                                </ul>
-                                <?php 
-                                    $arr=get_alives();
-                                    echo '<div class="pull-left">Participants: '.$arr[0].'</div><div class="pull-right"> Toujours en vie:'.$arr[1].'';
-                                ?>
-                            </div>
-                        </div>
+                        <?php include 'include/top5.php';?>
                     </div>
                 </div>
             </div>
