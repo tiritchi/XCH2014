a3e28047f739e41820f4647a856664eb1d00ef05
Cédric <tiritchi@gmail.com>
2014-02-20 00:43:38 +0100

    mise

--- get_pic.php
+++ get_pic.php
@@ -2,10 +2,15 @@
 session_start();
 if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SESSION['admin']) && $_SESSION['admin']=='FALSE')
 	{
-		$pseudo=$_GET['pseudo'];
 		if(isset($_GET['pseudo']) && $_GET['pseudo'] != ""){
-			if(!file_exists($pseudo.'.jpg')){
+			$pseudo=$_GET['pseudo'];
+			if(!file_exists('ressources/1/'.$pseudo.'.jpg')){
 				$pseudo='profil_resized';
+				$file = 'ressources/1/'.$pseudo.'.jpg';
+				$type = 'image/jpeg';
+				header('Content-Type:'.$type);
+				header('Content-Length: ' . filesize($file));
+				readfile($file);
 			}
 			else{
 				$file = 'ressources/1/'.$pseudo.'.jpg';
