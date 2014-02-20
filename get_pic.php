ac926f010454dad750b849e2460038175f3e3858
Cédric <tiritchi@gmail.com>
2014-02-20 16:30:08 +0100

    admin

--- get_pic.php
+++ get_pic.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SESSION['admin']) && $_SESSION['admin']=='FALSE')
+if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE')
 	{
 		if(isset($_GET['pseudo']) && $_GET['pseudo'] != ""){
 			$pseudo=$_GET['pseudo'];
