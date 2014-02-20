b18cc19ce1b184b3a038983180370cd724391462
Cédric <tiritchi@gmail.com>
2014-02-20 22:08:18 +0100

    correctifs

--- include/manager/board.php
+++ include/manager/board.php
@@ -25,8 +25,8 @@ if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SE
 				                		}
 				                		else {
 										    foreach ($array as $ar) {
-											    echo '<a href="#" class="list-group-item" data-toggle="modal" data-target="#'.$ar[3].'"><i class="pull-right glyphicon glyphicon-file"/></a>';
-											    echo $ar[3].'</a>';
+											    echo '<a href="#" class="list-group-item" data-toggle="modal" data-target="#'.$ar[3].'">';
+											    echo $ar[3].'<i class="glyphicon glyphicon-file"/></a>';
 											}
 										}
 									?>
