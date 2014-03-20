d39d60238acb2b6bd7cc94469d1536a52da71d37
Cédric PILLET <tiritchi@gmail.com>
2014-03-20 22:26:53 +0100

    conf

--- include/manager/admin/admin_board.php
+++ include/manager/admin/admin_board.php
@@ -31,7 +31,9 @@ if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SE
 											        <a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$i.'">
 											          '.substr($data['pseudo'],1,(strlen($data['pseudo'])-2));
 											          if ($data['confirmed']==0){
-												          		echo '<i class="glyphicon glyphicon-remove"></i>';
+												          		echo '<a class="pull-right" href="" data-toggle="modal" data-target="del'.$i.'"''>
+												          				<i class="glyphicon glyphicon-remove"></i>
+												          			  </a>';
 											          }
 
 											          echo '
