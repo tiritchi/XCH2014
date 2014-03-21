8ba24152cf31ac7ace294adfc710cec2532c5dc5
Cédric PILLET <tiritchi@gmail.com>
2014-03-22 00:41:26 +0100

    contrat

--- include/manager/admin/deletec.php
+++ include/manager/admin/deletec.php
@@ -11,8 +11,8 @@ if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SE
         <?php
         include 'footer.php';
         echo '<meta http-equiv="refresh" content="0; url=contrat">';
-        if(isset($_POST['idt']) && isset($_POST['idc']) && isset($_POST['date'])){
-	        create_contract($bdd,$_POST['idt'],$_POST['idc'],$_POST['date']);
+        if(isset($_GET['cno'])){
+	        delete_contract($bdd,$_GET['cno']);
     	}
     }
 elseif(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SESSION['admin']) && $_SESSION['admin']=='FALSE')
