fec4c39c5475b355e74e6591ceb213a3f3535608
Cédric PILLET <tiritchi@gmail.com>
2014-03-22 00:53:04 +0100

    contrat

--- deletec.php
+++ deletec.php
@@ -2,14 +2,14 @@
 session_start();
 if(isset($_SESSION['connected']) && $_SESSION['connected']=='TRUE' && isset($_SESSION['admin']) && $_SESSION['admin']=='TRUE')
     {
-    	include 'header.php';
+    	include 'include/manager/admin/header.php';
         $bdd=db_init();
         $contrats=get_contracts($bdd,"all");
 		?>
-		<?php include 'contrat_body.php'; ?>
+		<?php include 'include/manager/admin/contrat_body.php'; ?>
 
         <?php
-        include 'footer.php';
+        include 'include/manager/admin/footer.php';
         echo '<meta http-equiv="refresh" content="0; url=contrat">';
         if(isset($_GET['cno'])){
 	        delete_contract($bdd,$_GET['cno']);
