a2d80c81700349d7a8bd411f98b5a98bb79efcaf
Cédric PILLET <tiritchi@gmail.com>
2014-03-22 20:16:28 +0100

    board

--- include/lib/functions.php
+++ include/lib/functions.php
@@ -374,21 +374,29 @@
 	}
 
 	function save_db($table){
+		// Connect and query the database for the users
 		$bdd=db_init();
-		$file = 'export';
-		$val = $bdd->query("SELECT * FROM ".$table."");
-		while ($rowr = $val->fetch())
+		$sql = "SELECT * FROM XCH14_users ORDER BY pseudo";
+		$results = $conn->query($sql);
+		 
+		// Pick a filename and destination directory for the file
+		// Remember that the folder where you want to write the file has to be writable
+		$filename = "db_user_export_".time().".csv";
+		 
+		// Actually create the file
+		// The w+ parameter will wipe out and overwrite any existing file with the same name
+		$handle = fopen($filename, 'w+');
+		 
+		// Write the spreadsheet column titles / labels
+		//fputcsv($handle, array('Username','Email'));
+		 
+		// Write all the user records to the spreadsheet
+		foreach($results as $row)
 		{
-		for ($j=0;$j<$i;$j++)
-		{
-		$csv_output .= $rowr[$j]."; ";
+		    fputcsv($handle, array($row['fname'], $row['lname'], $row['mail'], $row['phone'], $row['adresse'], $row['date_naissance'], $row['pseudo'], $row['user_no'], $row['score']));
 		}
-		$csv_output .= "\n";
-		}
-		 
-		$filename = $file."_".date("Y-m-d_H-i",time());
-		 
-		print $csv_output;
-		exit;
+		 
+		// Finish writing the file
+		fclose($handle);
 	}
 ?>
